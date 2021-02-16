436fc16f90cdece297d4fd42b08766509ae21313
Ray Lin <nturay0321@gmail.com>
2021-02-16 12:50:38 +0800

    Add main page and register page functionality

--- app/src/main/java/com/example/shutthebox/Register.java
+++ app/src/main/java/com/example/shutthebox/Register.java
@@ -1,14 +1,121 @@
 package com.example.shutthebox;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
+
+    private static final String TAG = "TAG_REGISTER";
+    EditText email, password, displayName;
+    TextView backToLogin;
+    Button registerButton;
+    FirebaseAuth firebaseAuth;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email = findViewById(R.id.emailEditText);
+        password = findViewById(R.id.passwordEditText);
+        displayName = findViewById(R.id.createAccountNameEditText);
+        backToLogin = findViewById(R.id.back_to_login);
+        registerButton = findViewById(R.id.registerButton);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String _email = email.getText().toString().trim();
+                String _password = password.getText().toString().trim();
+                String _displayName = displayName.getText().toString().trim();
+
+                if (TextUtils.isEmpty(_email)) {
+                    email.setError("Email is required!");
+                }
+
+                if (_password.length() < 8) {
+                    password.setError("Password must be at least 8 digits!");
+                }
+
+                if (TextUtils.isEmpty(_displayName)) {
+                    displayName.setError("Display name is required!");
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(_email, _password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+                            firebaseUser.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(Register.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+                                }
+                            });
+
+                            Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            String userID = firebaseUser.getUid();
+                            DocumentReference documentReference = firebaseFirestore.collection("users").document(userID);
+                            Map<String, Object> userProfile = new HashMap<>();
+                            userProfile.put("email", _email);
+                            userProfile.put("display_name", _displayName);
+                            documentReference.set(userProfile).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d(TAG, "User profile Saved: " + userID);
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "Failed to save user profile: " + e.getMessage());
+                                }
+                            });
+
+                            startActivity(new Intent(getApplicationContext(), Lobby.class));
+                        } else {
+                            Toast.makeText(Register.this, "Failed to create user: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        backToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
     }
 }
